52da5faf2d36bc38a724a4d37ef9fecfb46fb020
RyanWH <ryan@propercursive.com>
2012-07-11 13:27:27 -0400

    init

---
+++ assets/js/custom.js
@@ -0,0 +1,34 @@
+function noisy(){
+	$('html,body').noisy({
+    intensity: 1, 
+    size: 200, 
+    opacity: 0.08,
+    fallback: '../img/noise.png',
+    monochrome: false
+	});
+}
+
+$(document).ready(function() {
+    noisy();
+    $(".navbar").hide();
+    $(".show-hide").show();
+    $('.show-hide').click(function(){
+        $(".navbar").slideToggle();
+    });
+    $('.flexslider').flexslider({
+    animation: "slide",
+    slideshow: false,
+    controlNav: false,
+    directionNav: false,
+    start: function(slider) {
+    $('.slide-anchor').click(function(event){
+      event.preventDefault();
+      var pos = parseInt(this.rel, 10);
+      if (pos !== slider.currentSlide) {
+        slider.flexAnimate(pos, true);
+        }
+      });
+      }
+      }
+    );
+})
