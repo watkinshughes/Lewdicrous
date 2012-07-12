83f38594d565dbb23ba05ba843e80eed178ccc8e
RyanWH <ryan@propercursive.com>
2012-07-12 13:37:49 -0400

    update gallery

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -33,18 +33,3 @@ $(document).ready(function() {
     );
 
 })
-
-$(".rslides").responsiveSlides({
-  auto: false,             // Boolean: Animate automatically, true or false
-  speed: 1000,            // Integer: Speed of the transition, in milliseconds
-  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
-  pager: true,           // Boolean: Show pager, true or false
-  nav: false,             // Boolean: Show navigation, true or false
-  random: false,          // Boolean: Randomize the order of the slides, true or false
-  pause: false,           // Boolean: Pause on hover, true or false
-  pauseControls: false,   // Boolean: Pause when hovering controls, true or false
-  prevText: "Previous",   // String: Text for the "previous" button
-  nextText: "Next",       // String: Text for the "next" button
-  maxwidth: "800",           // Integer: Max-width of the slideshow, in pixels
-  controls: "",           // Selector: Where controls should be appended to, default is after the 'ul'
-});
