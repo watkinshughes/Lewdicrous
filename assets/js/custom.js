326846fdaaf48209aeca6c90941cdc3a8fabe5d0
RyanWH <ryan@propercursive.com>
2013-02-10 09:01:45 -0500

    add ie fix for navbar

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,37 +1,34 @@
-function noisy(){
-	$('html,body').noisy({
-    intensity: 1, 
-    size: 200, 
-    opacity: 0.08,
-    fallback: '../img/noise.png',
-    monochrome: false
-	});
-}
-
-$(document).ready(function() {
-    $(".navbar").hide();
-    $(".show-hide").show();
-    $('.show-hide').click(function(){
-        $(".navbar").slideToggle();
+$(document).ready(function () {
+  function noisy() {
+    $('html,body').noisy({
+      intensity: 1,
+      size: 200,
+      opacity: 0.08,
+      fallback: '../img/noise.png',
+      monochrome: false
     });
-    noisy();
-    $('.flexslider').flexslider({
+  }
+  $(".navbar").hide();
+  $(".show-hide").show();
+  $('.show-hide').click(function () {
+    $(".navbar").slideToggle();
+  });
+  noisy();
+  $('.flexslider').flexslider({
     animation: "fade",
     slideshow: false,
     controlNav: false,
     directionNav: false,
-    touch: true,                  
+    touch: true,
     video: true,
-    start: function(slider) {
-    $('.slide-anchor').click(function(event){
-      event.preventDefault();
-      var pos = parseInt(this.rel, 10);
-      if (pos !== slider.currentSlide) {
-        slider.flexAnimate(pos, true);
-        }
+    start: function (slider) {
+      $('.slide-anchor').click(function (event) {
+          event.preventDefault();
+          var pos = parseInt(this.rel, 10);
+          if (pos !== slider.currentSlide) {
+            slider.flexAnimate(pos, true);
+          }
       });
-      }
-      }
-    );
-
-})
+    }
+  });
+});
